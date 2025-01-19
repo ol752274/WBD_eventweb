2d810bc0c7199c9f7b61b25e41b3c0c1d17497c1
ol752274 <ol752274@gmail.com>
2025-01-19 14:18:25 +0530

    all fixes

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeText = screen.getByText(/Welcome to Event Web/i);
+  expect(welcomeText).toBeInTheDocument();
+  
 });
