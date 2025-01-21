f4e8b61854055d15dcd41893a879263648168c6a
ol752274 <ol752274@gmail.com>
2025-01-21 11:16:54 +0530

    yml

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest"
-    },
-    moduleFileExtensions: ["js", "jsx", "json", "node"],
-    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  };
-  
+  testEnvironment: "jsdom", // Simulates a browser-like environment for React components
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest", // Transforms JavaScript and JSX files using Babel
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node"], // Recognized file extensions
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"], // Adds custom matchers for testing DOM elements
+  moduleNameMapper: {
+    "^.+\\.(css|scss|sass)$": "identity-obj-proxy", // Mocks CSS imports
+  },
+  moduleDirectories: ["node_modules", "src"], // Allows importing modules directly from 'src' without relative paths
+  collectCoverage: true, // Enables code coverage collection
+  collectCoverageFrom: ["src/**/*.{js,jsx}"], // Specifies files for which to collect coverage
+  coverageReporters: ["text", "lcov"], // Specifies the format of the coverage report
+};
